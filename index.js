f77b38c542dd4cc6da3bc75e6e92ebab1f6ebd6a
RunawayGOD <20022002utkarsh@gmail.com>
2023-04-01 19:43:13 +0530

    modified the index and user.json file

--- index.js
+++ index.js
@@ -1,39 +1,46 @@
-const express=require('express');
+const express = require("express");
+//JSON data import
+const { users } = require('./data/users.json');
 
-const app=express();
+const app = express();
 
-const PORT=8081;
+const PORT = 8081;
 
 app.use(express.json());
 
-app.get("/",(req,res)=>{
-    res.status(200).json({   //send and json does the same thing both send data in a json format
-        message:"server is running"
+app.get("/", (req, res) => {
+    res.status(200).json({
+        message: "The server is running",
+    });
+});
+
+/*
+* Route: /users
+* Method: GET
+* Description: Get all users
+* Access: public
+* Parameters: None
+*/
+app.get("/users", (req, res) => {
+    res.status(200).json({
+        success: true,
+        data: users,
     });
 });
 
 
+  
+   
 
 
 
 
-
-
-
-
-
-
-
-
-
-
-app.get("*",(req,res)=>{
-    res.status(404).json({
-        message:"This route does not exist"
+app.get("*", (req, res) => {
+    res.status(200).json({
+        message: "This route does not exist",
     });
 });
 
-
-app.listen(PORT,()=>{
-    console.log(`Server is running at port :> ${PORT}`);
+app.listen(PORT, () => {
+    console.log(`Server is running at port ${PORT}`);
 });
